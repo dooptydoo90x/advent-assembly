eb8702a9ed1699914f1ee13f5b13db0a5cde1b1a
Dustin Pyle <dooptydoo90x.dev@gmail.com>
2023-05-23 20:11:41 -0500

    idk something changed

--- y2015/d01/part1.s
+++ y2015/d01/part1.s
@@ -1,22 +1,16 @@
 bits 64
 global _start
 
-section .rodata
+section .data
 %include "input.s"
 %define UP, '('
 %define DOWN, ')'
-
-section .data
 
 section .bss
 
 section .text
 _start:
-        mov rax, 1
-        mov rdi, 1
-        mov rsi, input
-        mov rdx, input_len
-        syscall
+        
 
         mov rax, 60
         mov rdi, 0
